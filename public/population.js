e1b194307173fb9ba7ed3bda7266d5a92ceb0c13
Even Westvang <even@westvang.com>
2012-05-08 13:37:59 +0200

    Fixed js blooper

--- public/population.js
+++ public/population.js
@@ -1,8 +1,7 @@
 
 // SVG test taken from Modernizr 2.0
-if (! !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) {
+if (! (!!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)) {
   document.location = "stills";
-  return
 }
 
 var width = 700,
